f3f56f49b592ef0a2a784ca7df1fe657464679c1
dakwah <ceramahagama.id@gmail.com>
2016-07-30 14:10:18 +0700

    initial commit

---
+++ scripts/addtoany.js
@@ -0,0 +1,9 @@
+
+            var a2a_config = a2a_config || {};
+            a2a_config.num_services = 10;
+            a2a_config.prioritize = ["facebook", "twitter", "google_plus", "whatsapp", "skype", "line", "telegram", "sms", "wechat"];
+            a2a_config.color_main = "D7E5ED";
+            a2a_config.color_border = "eceeef";
+            a2a_config.color_link_text = "333333";
+            a2a_config.color_link_text_hover = "333333";
+
